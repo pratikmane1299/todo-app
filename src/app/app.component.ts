7f1918674da7abf96783832eeaf7379dd14eb741
pratik mane <pratik.mane1299@gmail.com>
2020-07-16 20:59:35 +0530

    :sparkles: complete todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+import { Todo } from './models/todo';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-app';
+  todo = '';
+  todos: Todo[] = [];
+
+  addTodo() {
+    if (this.todo === '') {
+      return;
+    }
+    const newTodo: Todo = {
+      id: this.todos.length + 1,
+      text: this.todo,
+      completed: false
+    };
+    this.todos.push(newTodo);
+    this.todo = '';
+  }
+
+  markTodoAsComplete(todo: Todo, index: number) {
+    const newTodos = [...this.todos];
+    newTodos.splice(index, 1, {
+      ...todo,
+      completed: !todo.completed
+    });
+
+    this.todos = newTodos;
+  }
+
+  removeTodo(todo: Todo) {
+    const updatedTodos = this.todos.filter(t => t.id !== todo.id);
+    this.todos = updatedTodos;
+  }
 }
